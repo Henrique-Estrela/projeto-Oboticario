11ac4d8b828d2b9d8ec4b96438cb7d835cc86f49
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-06-15 15:52:49 -0300

    confirm

--- _js/index.js
+++ _js/index.js
@@ -1,11 +1,1 @@
 
-  function mostrar(){
-  document.getElementById('login1').style.display = 'none';
-  document.getElementById('cadastro1').style.display = 'block';
-  };
-
-  function mostrar1(){
-    document.getElementById('cadastro1').style.display = 'none';
-    document.getElementById('login1').style.display = 'block';
-    };
-  
--- index.php
+++ index.php
@@ -21,23 +21,6 @@
       </div>
   </form>
 
-  <form class="form right_hover" id="cadastro1">
-      <h2 class="form__title">Cadastro</h2>
-      <p class="form__paragraph">Não tem uma conta? <a href="#login1" onclick=" mostrar1()" class="form__link1" id="login">Entre aqui</a></p>
-      <div class="form__container">
-          <div class="form__group">
-              <input type="text" id="user" class="form__input" placeholder=" ">
-              <label for="user" class="form__label">Usuario:</label>
-              <span class="form__line"></span>
-          </div>
-          <div class="form__group">
-              <input type="text" id="password" class="form__input" placeholder=" ">
-              <label for="password" class="form__label">Senha:</label>
-              <span class="form__line"></span>
-          </div>
-          <input type="submit" class="form__submit" value="Entrar">
-      </div>
-  </form>
 
 <?php include 'js.php' ?>
 <?php include 'footer.php' ?>
