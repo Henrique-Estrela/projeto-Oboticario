14d368bb2e07adbe10cd2bd8e2bda2f31f8a16a6
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-06-15 13:25:05 -0300

    atualização estrutura

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'head.php' ?>
+<?php include 'menu.php' ?>
+
+<?php include '' ?>
+<?php include 'js.php' ?>
+<?php include 'footer.php' ?>
---
+++ js.php
@@ -0,0 +1,1 @@
+<script src="_js/index.js"></script>
