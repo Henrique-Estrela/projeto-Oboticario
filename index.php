e867ffd029214cca425af2a3566ae036b6b7754a
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-06-15 15:24:53 -0300

    confirm

--- index.php
+++ index.php
@@ -2,9 +2,28 @@
 <?php include 'menu.php' ?>
 
 <div class="circle"></div>
-  <form class="form">
+
+  <form class="form left_hover" id="login1">
       <h2 class="form__title">Login</h2>
-      <p class="form__paragraph">Não tem uma conta? <a href="#" class="form__link">Entre aqui</a></p>
+      <p class="form__paragraph">Não tem uma conta? <a href="#cadastro" onclick="function att()" class="form__link1" id="login">Entre aqui</a></p>
+      <div class="form__container">
+          <div class="form__group">
+              <input type="num" id="cpf" class="form__input" onblur="validarCPF(this);"  maxlength ="14" placeholder=" " required>
+              <label for="user" class="form__label">CPF:</label>
+              <span class="form__line"></span>
+          </div>
+          <div class="form__group">
+              <input type="text" id="password" class="form__input" placeholder=" " required>
+              <label for="password" class="form__label">Senha:</label>
+              <span class="form__line"></span>
+          </div>
+          <input type="submit" class="form__submit" value="Entrar">
+      </div>
+  </form>
+
+  <form class="form right_hover" id="cadastro1">
+      <h2 class="form__title">Cadastro</h2>
+      <p class="form__paragraph">Não tem uma conta? <a href="#" class="form__link1" id="login">Entre aqui</a></p>
       <div class="form__container">
           <div class="form__group">
               <input type="text" id="user" class="form__input" placeholder=" ">
@@ -16,9 +35,9 @@
               <label for="password" class="form__label">Senha:</label>
               <span class="form__line"></span>
           </div>
-          <input type="submit" class="form__submit value=Entrar">
+          <input type="submit" class="form__submit" value="Entrar">
       </div>
   </form>
-  
+
 <?php include 'js.php' ?>
 <?php include 'footer.php' ?>
--- js.php
+++ js.php
@@ -1,1 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.8/jquery.mask.min.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script src="_js/index.js"></script>
+<script src="_js/cpf.js"></script>
