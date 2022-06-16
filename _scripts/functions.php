1f4267bc48889456d09c13fd9dfb8d5c4589edaf
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-06-16 15:34:22 -0300

    mexendo

--- _scripts/functions.php
+++ _scripts/functions.php
@@ -4,7 +4,6 @@
 
                 include "_scripts/config.php";
 
-               
                 $nome = $dados['nome'];
                 $dt_Cadastro = $dados['dt_Cadastro'];
                 $fornecedor = $dados['fornecedor'];
@@ -16,80 +15,20 @@
                 $sql = "INSERT INTO cad_Produto (nome_produto,fornecedor,valor_venda,custo_produto,data_cadastro,codigo_barra,quantidade) VALUES ('$nome','$fornecedor','$valor','$custo','$dt_Cadastro','$codigoBarra',$qntd)";
                 $query = $mysqli->query($sql);
 
-                if(isset($_POST)){
-    
-                    if(cadastro_produto($nome)==0){
-                        $sql = "INSERT INTO cad_Produto (nome_produto,fornecedor,valor_venda,custo_produto,data_cadastro) VALUES ('$nome','$fornecedor','$valor','$custo','$dt_Cadastro')";
-                        $query = $mysqli->query($sql);
-                        ?>
-    
-                        <script language='javascript'>
-                        Swal.fire({
-                        title: 'Salvo com sucesso meu patrão.',
-                        width: 600,
-                        padding: '3em',
-                        color: '#716add',
-                        background: '#fff url(/images/trees.png)',
-                        backdrop: `
-                            rgba(0,0,123,0.4)
-                            url("https://i.gifer.com/origin/51/518532f622d962be53e2e1f8989263a8_w200.gif")
-                            left top
-                            no-repeat
-                        `
-                        }).then((result) => {
-                            /* Read more about isConfirmed, isDenied below */
-                            if (result.isConfirmed) {
-                                window.location.href="painel.php?r=home";
-                            } else{
-                            window.location.href="painel.php?r=home";
-                            }
-                            })
-                        </script>
-                        <?php } else{?>
-                        
-                        <script language='javascript'>
-                        Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'Amigão, ce já fez o cadastro pae!',
-                        backdrop: `
-                            rgba(0,0,123,0.4)
-                            url("https://i.gifer.com/origin/51/518532f622d962be53e2e1f8989263a8_w200.gif")
-                            left top
-                            no-repeat
-                        `
-                        
-                        }).then((result) => {
-                        /* Read more about isConfirmed, isDenied below */
-                        if (result.isConfirmed) {
-                            window.location.href="painel.php?r=home";
-                        }
-                        })
-                        
-                        </script>
-                        <?php 
-                        }}
-                               
+                return $query;
 
-                return $query; 
+            }
+            //validação se ja foi 
 
-                
-            }
-            function cadastro_produto($nome){
-                include "config.php";
-                $sql = "SELECT nome FROM cad_Produto WHERE nome='$nome'";
-                $query = $mysqli->query($sql);
-                $total = mysqli_num_rows($query);
-                return $total;
-            }
+            // function cadastro_produto($nome){
+            //     include "config.php";
+            //     $sql = "SELECT nome FROM cad_Produto WHERE nome='$nome'";
+            //     $query = $mysqli->query($sql);
+            //     $total = mysqli_num_rows($query);
+            //     return $total;
+            // }  
 
         ?>
-<html lang="pt-br">
-    <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    </head>
-    <body>
-    <?php
+
 
     
--- views/cadProduto.php
+++ views/cadProduto.php
@@ -1,3 +1,4 @@
+<?php include "_scripts/functions.php"?>
 <div class="container-fluid form-aluno">
 <form class="row g-3" method="post" action="">
   <div class="col-md-6">
@@ -8,9 +9,13 @@
     <label class="form-label">Data de Cadastro</label>
     <input type="date" class="form-control" name="dt_Cadastro" required>
   </div>
-  <div class="col-12">
+  <div class="col-md-6">
     <label class="form-label">Codigo do Produto</label>
     <input type="text" class="form-control" name="codigoBarra" required>
+  </div>
+  <div class="col-md-6">
+    <label class="form-label">Quantidade</label>
+    <input type="number" class="form-control" name="qntd" required>
   </div>
   <div class="col-12">
     <label for="inputAddress2" class="form-label">Fornecedor</label>
@@ -23,12 +28,21 @@
   <div class="col-md-6">
     <label class="form-label">Custo</label>
     <input type="number" class="form-control" name="custo" required>
-  
   </div>
   <div class="col-12">
     <button type="submit" class="btn btn-primary">Salvar</button>
   </div>
 </form>
-<?php include "_scripts/functions.php"?>
+<?php
 
 
+if(isset($_POST)){
+
+  if(cadastrar($_POST)){
+    echo "Deu bom!";
+  }else{
+    echo "Me lasquei";
+  };
+}
+
+?>
