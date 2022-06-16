5d217b8a9e5c5c7af89d89feacca6876c7939a0f
Henrique Estrela <92654861+Henrique-Estrela@users.noreply.github.com>
2022-06-16 17:25:05 -0300

    fazendo

--- views/ControleV.php
+++ views/ControleV.php
@@ -1,3 +1,38 @@
+<form class="row g-3" method="post" action="">
+  <div class="col-md-6">
+    <label  class="form-label">Nome do Produto</label>
+    <input type="text" class="form-control" name="nome" required>
+  </div>
+  <div class="col-md-6">
+    <label class="form-label">Data de Cadastro</label>
+    <input type="date" class="form-control" name="dt_Cadastro" required>
+  </div>
+  <div class="col-md-6">
+    <label class="form-label">Codigo do Produto</label>
+    <input type="text" class="form-control" name="codigoBarra" required>
+  </div>
+  <div class="col-md-6">
+    <label class="form-label">Quantidade</label>
+    <input type="number" class="form-control" name="qntd" required>
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Fornecedor</label>
+    <input type="text" class="form-control" name="fornecedor" required>
+  </div>
+  <div class="col-md-6">
+    <label class="form-label">Valor</label>
+    <input type="number" class="form-control" name="valor" required>
+  </div>
+  <div class="col-md-6">
+    <label class="form-label">Custo</label>
+    <input type="number" class="form-control" name="custo" required>
+  </div>
+  <div class="col-12">
+    <button type="submit" class="btn btn-primary">Salvar</button>
+  </div>
+</form>
+<?php include "_scripts/functions.php"; ?>
+
 <section id="listar_clientes">
   <div class="container-fluid">
     <div class="row">
