c9f8557c564fab31335a3765548b0803293bc867
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-06-16 11:51:41 -0300

    arquivo atualizado

--- _scripts/functions.php
+++ _scripts/functions.php
@@ -6,12 +6,11 @@
 
         $nome = $dados['nome'];
         $dt_Cadastro = $dados['dt_Cadastro'];
-        $codigo = $dados['codigo'];
         $fornecedor = $dados['fornecedor'];
         $valor = $dados['valor'];
         $custo = $dados['custo'];
 
-        $sql = "INSERT INTO cadProduto (aluno,codigo,fornecedor,valor,custo,dt_Cadastro) VALUES ('$nome','$codigo','$fornecedor','$valor','$custo','$dt_Cadastro')";
+        $sql = "INSERT INTO cad_Produto (nome_produto,fornecedor,valor_venda,custo_produto,data_cadastro) VALUES ('$nome','$fornecedor','$valor','$custo','$dt_Cadastro')";
         $query = $mysqli->query($sql);
 
         return $query;
